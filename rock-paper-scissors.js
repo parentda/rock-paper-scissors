87c73c80a3e9cdaebc9d04d5cac7979a3b3db2db
parentda <danielparent21@gmail.com>
2021-06-19 15:04:38 -0400

    Added computerPlay function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,1 +1,23 @@
-console.log("this is a test");
+function computerPlay() {
+  let randomSelection = Math.floor(Math.random() * 3);
+  let computerSelection;
+
+  switch (randomSelection) {
+    case 0:
+      computerSelection = "Rock";
+      break;
+    case 1:
+      computerSelection = "Paper";
+      break;
+    case 2:
+      computerSelection = "Scissors";
+      break;
+    default:
+      console.error("Something went wrong");
+      break;
+  }
+  console.log(computerSelection);
+  return computerSelection;
+}
+
+computerPlay();
