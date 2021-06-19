97967f9d986a7acf87b95582679e2469c0fc7e3b
parentda <danielparent21@gmail.com>
2021-06-19 15:47:30 -0400

    Refactored computerPlay function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,23 +1,23 @@
 function computerPlay() {
+  let computerOptions = ["Rock", "Paper", "Scissors"];
   let randomSelection = Math.floor(Math.random() * 3);
-  let computerSelection;
-
-  switch (randomSelection) {
-    case 0:
-      computerSelection = "Rock";
-      break;
-    case 1:
-      computerSelection = "Paper";
-      break;
-    case 2:
-      computerSelection = "Scissors";
-      break;
-    default:
-      console.error("Something went wrong");
-      break;
-  }
-  console.log(computerSelection);
-  return computerSelection;
+  return computerOptions[randomSelection];
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+  playerSelection =
+    playerSelection.charAt(0).toUpperCase() +
+    playerSelection.slice(1).toLowerCase();
+
+  // if (playerSelection === computerSelection) {
+  //   return "It's a tie!";
+  // }
+  // else if ()
+}
+
+const playerSelection = "rOcK";
+const computerSelection = computerPlay();
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
