08dd28e9c1402264d56876320fb2e99bdadd80a0
parentda <danielparent21@gmail.com>
2021-06-20 14:30:19 -0400

    Added userPlay and getRounds functions

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,6 +5,23 @@ function computerPlay() {
   return gameOptions[randomSelection];
 }
 
+function userPlay() {
+  let userInput = prompt('Please enter "Rock", "Paper", or "Scissors"').trim();
+  return userInput.charAt(0).toUpperCase() + userInput.slice(1).toLowerCase();
+}
+
+function getRounds() {
+  let numRounds = parseInt(
+    prompt(
+      "How many rounds would you like to play? (Please enter an integer greater than 0)"
+    )
+  );
+  if (!Number.isInteger(numRounds) || numRounds <= 0) {
+    numRounds = getRounds();
+  }
+  return numRounds;
+}
+
 function playRound(playerSelection, computerSelection) {
   // eslint-disable-next-line no-param-reassign
   playerSelection =
@@ -16,17 +33,28 @@ function playRound(playerSelection, computerSelection) {
     gameOptions.indexOf(computerSelection);
 
   if (indexDiff === 0) {
-    return `It's a Tie!`;
+    console.log(`It's a Tie!`);
+    return 0;
   }
   if (indexDiff === 1 || indexDiff === -2) {
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+    console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+    return 1;
   }
-  return `You Lose! ${computerSelection} beats ${playerSelection}`;
+  console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+  return -1;
 }
 
-const playerSelection = "paper";
-const computerSelection = computerPlay();
+function game() {
+  const playerSelection = "paper";
+  let keepScore = 0;
+  for (let i = 0; i < rounds; i++) {
+    const computerSelection = computerPlay();
+    keepScore += playRound(playerSelection, computerSelection);
+    console.log(keepScore);
+  }
+}
 
-console.log(playerSelection);
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+// console.log(playerSelection);
+// console.log(playRound(playerSelection, computerSelection));
+// console.log(computerSelection);
+console.log(getRounds());
